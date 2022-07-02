672a5fb646c33185c38fc6f79339f9349d046b9a
Kang Kyunghyun <kangkh0906@khu.ac.kr>
2022-07-02 17:57:31 +0900

    [ADD] class (declaration, getter, setter, inheritance etc)

---
+++ notes/class.js
@@ -0,0 +1,126 @@
+'use strict';
+// Object-oriented programming
+// class: template
+// object: instance of a class
+// JavaScript classes
+//  - introduced in ES6
+//  - syntactical sugar over prototype-based inheritance
+
+// 1. Class declarations
+class Person {
+    // constructor
+    constructor(name, age) {
+        // fields
+        this.name = name;
+        this.age = age;
+    }
+    
+    // methods
+    speak() {
+        console.log(`${this.name}: hello!`);
+    }
+}
+
+const sam = new Person('Sam', 20);
+console.log(sam.name); // Sam
+console.log(sam.age); // 20
+sam.speak(); // Sam: hello!
+
+// 2. Getter and Setter
+class User {
+    constructor(firstName, lastName, age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    get age() {
+        return this._age;
+    }
+
+    set age(value) {
+        // if (value < 0) {
+        //     throw Error('age can not be negative');
+        // }
+        this._age = value < 0 ? 0 : value;
+    }
+}
+
+const user1 = new User('Steve', 'Job', -1);
+console.log(user1.age); // 0
+
+// 3. Fields (public, private)
+// Too soon!
+class Experiment {
+    publicField = 2;
+    #privateField = 0;
+}
+const experiment = new Experiment();
+console.log(experiment.publicField); // 2
+console.log(experiment.privateField); // undefined
+
+// 4. Static properties and methods
+// Too soon!
+class Article {
+    static publisher = 'My Name';
+    constructor(articleNumber) {
+        this.articleNumber = articleNumber;
+    }
+
+    static printPublisher() {
+        console.log(Article.publisher);
+    }
+}
+
+const article1 = new Article(1);
+const article2 = new Article(2);
+console.log(Article.publisher); // My Name
+Article.printPublisher(); // My Name
+
+// 5. Inheritance
+// a way for one class to extend another class.
+class Shape {
+    constructor(width, height, color) {
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+
+    draw() {
+        console.log(`drawing ${this.color} color!`);
+    }
+
+    getArea() {
+        return this.width * this.height;
+    }
+}
+
+class Rectangle extends Shape {}
+class Triangle extends Shape {
+    draw() { // Overwriting
+        super.draw();
+        console.log('🔺');
+    }
+    getArea() {
+        return (this.width * this.height) / 2;
+    }
+
+    toString() {
+        return `Triangle: color: ${this.color}`;
+    }
+}
+
+const rectangle = new Rectangle(20, 20, 'blue');
+rectangle.draw(); // drawing blue color!
+console.log(rectangle.getArea()); // 400
+const triangle = new Triangle(20, 20, 'red');
+triangle.draw(); // drawing red color!, 🔺
+console.log(triangle.getArea()); // 200
+
+// 6. Class checking: instanceOf
+console.log(rectangle instanceof Rectangle); // true
+console.log(triangle instanceof Rectangle); // false
+console.log(triangle instanceof Triangle); // true
+console.log(triangle instanceof Shape); // true
+console.log(triangle instanceof Object); // true
+console.log(triangle.toString()); // Triangle: color: red
