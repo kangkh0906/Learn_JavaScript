a2c585fb2f12ca5f515cc72cf610e4d069a0f230
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2022-08-15 23:03:11 +0900

    [UPDATE] c++ basic - add cin

--- cpp-notes/basic.cpp
+++ cpp-notes/basic.cpp
@@ -2,19 +2,22 @@
 #include <iostream>
 
 // 2. using namespace std;
-// std::cout -> cout (std 생략 가능하도록)
-// 3. ';' 종결자 -> 종결자 기준으로 코드 실행
+// - std::cout -> cout (std:: 생략 가능하도록)
 using namespace std;
+// 3. 종결자 ;
+// - 종결자 기준으로 코드 실행
 
 // 4. C++ 코드에는 반드시 main의 이름을 가지고 있는 함수가 있어야 한다.
 int main()
 {
-  // 5. cout == 출력 함수 c out
-  // endl == \n (줄바꿈) end line
-  // << == 데이터 방향 표시
+  // 5. 출력 cout
+  // 6. 입력 cin
+  // 7. 줄바꿈 endl
+  // 8. 데이터 흐름 방향 표시 <<
   cout << "Hello, World!" << endl;
+  // cin >>
 
-  // 6. return 0
-  // 함수를 성공적으로 마침을 알림
+  // . return 0
+  // - 함수를 성공적으로 마침을 알림
   return 0;
 }
