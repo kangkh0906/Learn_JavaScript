26a2a73b74ef406e5fab7ff90bdd0875e84a90bc
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2022-08-15 16:42:59 +0900

    [ADD] C++ Basic

---
+++ cpp-notes/basic.cpp
@@ -0,0 +1,20 @@
+// 1. 전처리 지시자
+#include <iostream>
+
+// 2. using namespace std;
+// std::cout -> cout (std 생략 가능하도록)
+// 3. ';' 종결자 -> 종결자 기준으로 코드 실행
+using namespace std;
+
+// 4. C++ 코드에는 반드시 main의 이름을 가지고 있는 함수가 있어야 한다.
+int main()
+{
+  // 5. cout == 출력 함수 c out
+  // endl == \n (줄바꿈) end line
+  // << == 데이터 방향 표시
+  cout << "Hello, World!" << endl;
+
+  // 6. return 0
+  // 함수를 성공적으로 마침을 알림
+  return 0;
+}
