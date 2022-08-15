a192dea6b0efdc24a083b2d8069aa01348811186
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2022-08-15 22:21:34 +0900

    [FIX] operator description fix

--- cpp-notes/operator.cpp
+++ cpp-notes/operator.cpp
@@ -6,9 +6,8 @@ using namespace std;
 
 int main()
 {
-  // + - * / % -> 산술 연산자
-  // 두 개의 값이 필요 -> 이 두 값을 피연산자
-
+  // 1. 산술 연산자 + - * / %
+  // - 두 개의 값이 필요 (이 두 값을 피연산자)
   // + : add
   // - : subtract
   // * : multiply
@@ -30,7 +29,7 @@ int main()
   cout << "f: " << f << endl;
   cout << "g: " << g << endl;
 
-  // c++는 일반적 산술 순서를 따름
+  // - c++는 일반적 산술 순서를 따름
   int h = 10;
   int i = 3;
   int j = 5;
@@ -41,7 +40,7 @@ int main()
   int divide = h / i * j; // / -> *
   cout << divide << endl; // 15
 
-  // 하나라도 실수형이면 결과는 실수형
+  // - 하나라도 실수형이면 결과는 실수형
   float k = 9.0;
   int l = 5;
   cout << k / l << endl; // 1.8
